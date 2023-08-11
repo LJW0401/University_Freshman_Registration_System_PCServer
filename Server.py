7883e46728e0b0fccc166ee46e5a3df15d210aee
是小企鹅呀 <1357482552@qq.com>
2023-08-11 22:23:43 +0800

    继续添加功能

--- Server.py
+++ Server.py
@@ -1,4 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import pandas as pd
+import os
+import sys
 
 def description():
     '''
@@ -15,15 +18,46 @@ def description():
     '''
     return
     
-
+#所有数据均以字符串储存
 app = Flask(__name__)
+needful_infomation = [] # 需要提交的材料信息列表
+needful_infomation = ['户口本','毕业证','学位证']#,'照片','体检表','党团关系证明','学籍证明','学生证']
+data_dict_initial = {
+    '姓名':[],#默认列
+    '身份证号':[],#默认列
+    '学号':[],#默认列
+    '户口本':[],
+    '毕业证':[],
+    '学位证':[]
+}
+data_frame = pd.DataFrame(data_dict_initial) # 数据表格
+print(data_frame)
 
+def AppPath():
+    """获得主程序的路径"""
+    """Returns the base application path."""
+    if hasattr(sys, 'frozen'):
+        return os.path.dirname(sys.executable)  #使用pyinstaller打包后的exe目录
+    return os.path.dirname(__file__)            #没打包前的py目录
+
+
+
+def Init():
+    '''初始化'''
+    return
+    
+
+def Backup():
+    '''备份数据'''
+    data_frame.to_excel(AppPath()+'/backup.xlsx', sheet_name='Sheet1')   
+    return
 
 
 # 这里是通用服务
 @app.route('/connection_test', methods=['GET'])
 def ConnectionTest():
     '''用于检测服务连接情况，如果连接上了则会返回connedted'''
+    print('需要提交的材料信息：',needful_infomation)
     return "connected"
 
 
@@ -31,22 +65,100 @@ def ConnectionTest():
 @app.route('/get_needful_upload_infomation', methods=['GET'])
 def GetNeedfulUploadInfomation():
     '''用于获取需要提交的材料的信息，即具体有哪些材料需要提交'''
-    return "is developing..."
+    global needful_infomation
+    print('客户端请求获取需要提交的材料类型')
+    data = {}
+    for i,info_name in enumerate(needful_infomation):
+        data[f'info{i}'] = info_name
+    return jsonify(data)
     
     
 @app.route('/upload_infomation', methods=['POST'])
 def UploadInfomation():
     '''用于接收客户端上传的收集材料信息'''
-    data = request.json  # 获取 POST 请求中的 JSON 数据
-    print(data)
-    return f"This is a POST request with JSON data: {data}"
+    global data_frame
+    json = request.json  # 获取 POST 请求中的 JSON 数据
+    print(json)
+    name = json['姓名']
+    ID_number = json['身份证号']
+    student_ID = json['学号']
+    if ID_number != '':
+        if ID_number in data_frame['身份证号'].tolist():
+            row_index = data_frame[data_frame['身份证号']==ID_number].index.tolist()[0]# 获取该身份证对应的第一行该行的索引
+            UpdateRow(row_index, json)
+        else:
+            AddNewRow(json)
+    elif student_ID != '':
+        if student_ID in data_frame['学号']:
+            row_index = data_frame[data_frame['学号']==student_ID].index.tolist()[0]# 获取该学号对应的第一行该行的索引
+            UpdateRow(row_index, json)
+        else:
+            AddNewRow(json)
+    else:
+        return 'Can_not_identify'
+    
+    Backup()
+    print(data_frame)
+    return "Received"
 
 
-# 获取已提交的信息
+def UpdateSheet(row_index, json):
+    '''弃用'''
+    raise RuntimeError('该方法已弃用')
+    global data_frame
+    columns = data_frame.columns.tolist()
+    for column in columns:
+        if column in json:
+            if json[column] != data_frame.loc[row_index, column]:
+                data_frame.loc[row_index, column] = json[column]
+    
+    
+def UpdateRow(row_index, json):
+    '''更新行内容'''
+    global data_frame
+    columns = data_frame.columns.tolist()
+    for column in columns:
+        if column in json:
+            if json[column] != data_frame.loc[row_index, column]:
+                data_frame.loc[row_index, column] = json[column]
+
+
+def AddNewRow(json):
+    '''添加一行新数据'''
+    global data_frame
+    new_row = {}
+    for data in json:
+        new_row[data] = json[data]
+    new_row_index = data_frame.shape[0]
+    data_frame.loc[new_row_index] = new_row
+
+
 @app.route('/get_infomation', methods=['GET', 'POST'])
 def GetInfomation():
+    '''TODO:测试可靠性'''
     '''通过对象信息获取该对象目前已经提交的信息'''
-    return "This is a GET request."
+    global data_frame
+    json = request.json  # 获取 POST 请求中的 JSON 数据
+    print(json)
+    name = json['姓名']
+    ID_number = json['身份证号']
+    student_ID = json['学号']
+    if ID_number != '':
+        if ID_number in data_frame['身份证号'].tolist():
+            row_index = data_frame[data_frame['身份证号']==ID_number].index.tolist()[0]# 获取该身份证对应的第一行该行的索引
+            return_json = data_frame.iloc[row_index].to_dict()
+            return jsonify(return_json)
+        else:
+            return 'No_data'
+    elif student_ID != '':
+        if student_ID in data_frame['学号']:
+            row_index = data_frame[data_frame['学号']==student_ID].index.tolist()[0]# 获取该学号对应的第一行该行的索引
+            return_json = data_frame.iloc[row_index].to_dict()
+            return jsonify(return_json)
+        else:
+            return 'No_data'
+    else:
+        return 'Can_not_identify'
 
 
 # 与本机服务可视化界面进行数据交互
@@ -55,17 +167,34 @@ def GetDataSheet():
     '''获取所有对象材料收集情况的数据表格'''
     return "This is a GET request."
 
-@app.route('/back_up', methods=['GET'])
-def back_up():
-    '''备份数据信息'''
-    return "Data have been backed up."
 
-def init():
-    '''初始化'''
-    return
-    
-    
+@app.route('/recovery', methods=['PSOT'])
+def Recovery():
+    '''恢复备份的数据信息'''
+    return "Data have been recovered."
+
+
+@app.route('/set', methods=['POST'])
+def Set():
+    '''TODO:根据更新的信息对原有的列进行增减'''
+    '''设置需要提交的材料信息'''
+    global needful_infomation
+    needful_infomation_tmp = request.json['needful_info']
+    for nf in needful_infomation_tmp:
+        if nf not in needful_infomation:
+            needful_infomation.append(nf)
+            #TODO（小企鹅）：在数据表格中增加一列
+    for nf in needful_infomation:
+        if nf not in needful_infomation_tmp:
+            needful_infomation.append(nf)
+            #TODO（小企鹅）：在数据表格中删除一列
+    needful_infomation = needful_infomation_tmp.copy()
+    return "Server is setted."
+
+
+
+
 if __name__ == '__main__':
-    init()
+    Init()
     print('\n\n\n欢迎使用新生报到材料收集系统！\n\n\n')
     app.run(host="0.0.0.0",port=5000)#,debug=True)
--- ServerView.py
+++ ServerView.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import tkinter.ttk
 import tkinter.messagebox
 import requests
+import pandas as pd
 
 def GetLocalIP():
     # 获取本机主机名
@@ -15,6 +16,7 @@ def GetLocalIP():
 # print("本机IP地址:", local_ip)
 
 MAX_TIMEOUT = 1 # 最大超时时间(s)
+MAX_INFOMATION = 15 # 最大信息个数
 
 CONNECTED = 0
 DISCONNECTED = 1
@@ -23,11 +25,18 @@ class Server_View():
     def __init__(self) -> None:
         self.host_ip = GetLocalIP()
         self.port = '5000'
+        self.version = 'v1.0.0-alpha'
 
         self.window = tk.Tk()
-        self.window.title('新生报到材料辅助收集系统服务端')
+        self.window.title(f'新生报到材料辅助收集系统服务端 {self.version}')
         self.window.geometry('700x600')
         self.window.resizable(0, 0)
+        
+        self.needful_infomation = [ # 需要提交的材料信息列表
+            '户口本','毕业证','学位证','照片','体检表','党团关系证明','学籍证明','学生证'
+            ]
+        self.connect_state = DISCONNECTED # 连接状态
+        
         
         self.CreateWidgets()
     
@@ -43,15 +52,15 @@ class Server_View():
         self.Frame_ConnectionTest.place(x=10, y=5, width=210, height=110)
         self.CreateWidgets_Frame_ConnectionTest()
         
-        self.Frame_DataTable = tk.LabelFrame(
+        self.Frame_Settings = tk.LabelFrame(
             self.window,
             text="设置相关信息",
             font=('微软雅黑',10),
             bd=2,
             relief="groove",
             )
-        self.Frame_DataTable.place(x=230, y=5, width=460, height=110)
-        self.CreateWidgets_Frame_DataTable()
+        self.Frame_Settings.place(x=230, y=5, width=460, height=110)
+        self.CreateWidgets_Frame_Settings()
         
         self.Frame_OperationBar = tk.LabelFrame(
             self.window,
@@ -76,18 +85,26 @@ class Server_View():
     def CreateWidgets_Frame_OperationBar(self):
         button_width = 105
         button_height = 30
-        self.Button_UpdateDataTable = tk.Button(#用于的更新数据表格按钮
+        self.Button_UpdateDataTable = tk.Button(#用于更新数据表格的按钮
             self.Frame_OperationBar,
             text='更新数据表格',
             font=('微软雅黑',10),
-            command=self.Button_ConnectToServer_Click,
+            command=self.BlankFunction,
             )
         self.Button_UpdateDataTable.place(x=5, y=0, width=button_width, height=button_height)
         
+        self.Button_Recover = tk.Button(#用于备份恢复的按钮
+            self.Frame_OperationBar,
+            text='恢复备份数据',
+            font=('微软雅黑',10),
+            command=self.BlankFunction,
+            )
+        self.Button_Recover.place(x=5, y=int(button_height*1.2), width=button_width, height=button_height)
         
-    def CreateWidgets_Frame_DataTable(self):
+        
+    def CreateWidgets_Frame_Settings(self):
         self.LabelPrompt_NeedfulInfomation = tk.Label(#用于显示提示需要提交的材料类型
-            self.Frame_DataTable,
+            self.Frame_Settings,
             text='需要提交的材料类型：',
             font=('微软雅黑',10),
             anchor='w',
@@ -95,33 +112,43 @@ class Server_View():
         self.LabelPrompt_NeedfulInfomation.place(x=5, y=0, width=130, height=20)
 
         self.Combobox_NeedfulInfomation = tkinter.ttk.Combobox(#用于创建、增删需要提交的信息
-                        self.Frame_DataTable,
+                        self.Frame_Settings,
                         width=10,height=1,
-                        font=('Arial', 12),
-                        values=[],
+                        font=('微软雅黑', 10),
+                        values=self.needful_infomation,
                         state='normal'
                     )
         self.Combobox_NeedfulInfomation.place(x=140, y=0, width=120, height=20)
-        # self.Combobox_SendData_Name.bind("<KeyRelease>", self.Combobox_SendData_Name_OnKeyRelease)
-        # self.Combobox_SendData_Name.bind("<Control-KeyPress-Delete>", self.Combobox_SendData_Name_OnCtrlDel)
-        # self.Combobox_SendData_Name.bind("<<ComboboxSelected>>", self.Combobox_SendData_Name_Selected)
+        if self.needful_infomation.__len__()>0:
+            self.Combobox_NeedfulInfomation.set(self.needful_infomation[0])
+        self.Combobox_NeedfulInfomation.bind("<Return>", self.Combobox_NeedfulInfomation_OnReturn)#按回车键增加内容
+        self.Combobox_NeedfulInfomation.bind("<Escape>", self.Combobox_NeedfulInfomation_OnEscape)#按esc键删除内容
         
+        self.Button_UploadToServer = tk.Button(#用于将需要提交的信息等设置上传到服务中
+            self.Frame_Settings,
+            text='上传至服务器',
+            font=('微软雅黑',10),
+            command=self.Button_UploadToServer_Click,
+            )
+        self.Button_UploadToServer.place(x=270, y=-5, width=90, height=30)
+
         self.LabelPrompt_NameList = tk.Label(#用于显示提示电子名单的路径
-            self.Frame_DataTable,
+            self.Frame_Settings,
             text='电子名单路径：',
             font=('微软雅黑',10),
             anchor='w',
             )
         self.LabelPrompt_NameList.place(x=5, y=30, width=130, height=20)
-        
+
         self.Entry_NameList = tk.Entry(#用于输入电子名单的路径
-            self.Frame_DataTable,
+            self.Frame_Settings,
             font=('微软雅黑',10),
+            state='readonly',
             )
         self.Entry_NameList.place(x=100, y=30, width=350, height=20)
-        
+
         self.LabelPrompt_NameList = tk.Label(#用于显示提示电子照片文件夹的路径
-            self.Frame_DataTable,
+            self.Frame_Settings,
             text='电子照片文件夹路径：',
             font=('微软雅黑',10),
             anchor='w',
@@ -129,10 +156,45 @@ class Server_View():
         self.LabelPrompt_NameList.place(x=5, y=60, width=130, height=20)
         
         self.Entry_NameList = tk.Entry(#用于输入电子照片文件夹的路径
-            self.Frame_DataTable,
+            self.Frame_Settings,
             font=('微软雅黑',10),
+            state='readonly',
             )
         self.Entry_NameList.place(x=140, y=60, width=310, height=20)
+    
+    
+    def Combobox_NeedfulInfomation_OnEscape(self,event):#用于删除发送数据
+        '''当按下Esc键的时候删除需要提交的信息'''
+        index = self.Combobox_NeedfulInfomation.current()
+        if index == -1:#如果在数据列表中找不到则不进行任何操作直接返回
+            pass
+            # return
+        
+        self.needful_infomation.pop(index)
+        self.Combobox_NeedfulInfomation.configure(values=self.needful_infomation)
+        
+        if self.needful_infomation.__len__() == 0:#如果没数据了就设为空
+            self.Combobox_NeedfulInfomation.set('')
+        elif index == self.needful_infomation.__len__():#如果删除的是最后一个数据则跳转到最后一个数据
+            self.Combobox_NeedfulInfomation.set(self.needful_infomation[self.needful_infomation.__len__()-1])#删除后跳转到第一个数据
+        else:
+            self.Combobox_NeedfulInfomation.set(self.needful_infomation[index])
+        # print(self.Combobox_NeedfulInfomation.current())
+        # print(index)
+        # print(self.needful_infomation)
+    
+        
+    def Combobox_NeedfulInfomation_OnReturn(self,event):#用于输入内容及检测是否添加数据
+        '''当按下回车键的时候添加需要提交的信息到列表中'''
+        if self.Combobox_NeedfulInfomation.get() == '':
+            tkinter.messagebox.showwarning(title='提示', message='需要提交的信息不能为空！')
+        else:
+            if self.Combobox_NeedfulInfomation.current()==-1 and self.needful_infomation.__len__()<MAX_INFOMATION:#发送数据名称不存在则添加新的发送数据
+                info_name = self.Combobox_NeedfulInfomation.get()
+                self.needful_infomation.append(info_name)
+                
+                self.Combobox_NeedfulInfomation.configure(values=self.needful_infomation)
+            
         
         
     def CreateWidgets_Frame_ConnectionTest(self):
@@ -175,21 +237,40 @@ class Server_View():
         self.Label_Port.place(x=5, y=55, width=180, height=20)
         
     
+    def Button_UploadToServer_Click(self):
+        '''TODO:完成将相关设置传递到服务器的功能'''
+        if self.connect_state == DISCONNECTED:
+            tkinter.messagebox.showwarning(title='提示', message='服务未开启！')
+            return
+        elif self.connect_state == CONNECTED:
+            json_data = {'needful_info':self.needful_infomation}
+            response = requests.post(
+                f'http://127.0.0.1:5000/set',
+                json=json_data,
+                timeout=MAX_TIMEOUT
+                )
+            print(response.text)
+    
+    
     def Button_ConnectToServer_Click(self):
         self.host_ip = GetLocalIP()
         self.Label_HostIP['text'] = f'服务器IP：{self.host_ip}'
-        response = requests.get(
-            f'http://127.0.0.1:5000/connection_test',
-            timeout=MAX_TIMEOUT
-            )
-        if response.status_code==200:
-            if response.text=='connected':
-                self.connect_state = CONNECTED
-        else:
+        try:
+            response = requests.get(
+                f'http://127.0.0.1:5000/connection_test',
+                timeout=MAX_TIMEOUT,
+                )
+            if response.status_code==200:
+                if response.text=='connected':
+                    self.connect_state = CONNECTED
+            else:
+                self.connect_state = DISCONNECTED
+            self.SetConnectState()
+            if self.connect_state == CONNECTED:
+                self.window.after(5000, self.Button_ConnectToServer_Click)#每隔xs自动检测一次连接状态
+        except:
             self.connect_state = DISCONNECTED
-        self.SetConnectState()
-        if self.connect_state == CONNECTED:
-            self.window.after(5000, self.Button_ConnectToServer_Click)#每隔xs自动检测一次连接状态
+            self.SetConnectState()
         
         
     def SetConnectState(self):
